2e3ce080b5b0f0c63bdc6e7e5161f2d7f1a3eb53
Liang C <liangc@ospray.bluesky>
2015-12-01 21:54:39 -0500

    adding demo client

---
+++ binder/tests/demoClient.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <binder/IServiceManager.h>
+#include "IDemoInterface.h"
+
+using namespace std;
+using namespace android;
+using namespace my_test;
+
+
+int main(int argc, char * argv[])
+{
+    pid_t pid = getpid();
+    uid_t uid = getuid();
+    cout << "demo client, pid: " << pid << ", uid: " << uid << endl;
+
+    sp<IServiceManager> svcMgr = defaultServiceManager();
+    sp<IBinder> binder = svcMgr->getService(String16(IDemoInterface::MY_IFACE_NAME));
+
+    cout << "demo iface binder := " << binder.get() << endl;
+
+    sp<IDemoInterface> demoIface = IDemoInterface::asInterface(binder);
+
+    cout << "demoIface := " << demoIface.get() << endl;
+
+    cout << "bye!" << endl;
+}
